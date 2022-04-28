8ab74d37db461545c2ea2c450ea58506abb27074
Alexandr Maikevich <maikevich@mail.ru>
2022-04-28 19:55:55 +0200

    steam login added

---
+++ src/main/java/com/example/webparser/util/SeleniumConnector.java
@@ -0,0 +1,23 @@
+package com.example.webparser.util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class SeleniumConnector {
+
+
+
+    public WebDriver connect() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        return new ChromeDriver(options);
+    }
+
+
+}
